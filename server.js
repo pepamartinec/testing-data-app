e043e14cb6da4620aa38701814692eb8ab3cb898
Pepa Martinec <josef.martinec@keboola.com>
2026-05-05 08:52:24 +0200

    test

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.get('/__env', (req, res) => {
 
     const content = `
   <h2>Environment Variables</h2>
-  <h3>test2</h3>
+  <h3>test3</h3>
   <div class="section">
     ${rows ? `<table>${rows}</table>` : '<span class="empty">(none)</span>'}
   </div>`;
